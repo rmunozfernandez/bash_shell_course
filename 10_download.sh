9406dfa98a7bdf0414618cc6ca81aecec3eadaee
rmunozfernandez <rmunoz.it@gmail.com>
2024-04-27 11:33:31 -0400

    Descargar información de Internet

---
+++ 10_download.sh
@@ -0,0 +1,5 @@
+# !/bin/bashi
+# Programa para ejemplificar el uso de la descarga de información desde internet utilizando el comando wget
+
+echo "Descargar información de internet"
+wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35-windows-x64.zip
